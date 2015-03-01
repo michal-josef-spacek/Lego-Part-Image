b68d49d979891f49cba886bd0f8e06e2dcf00b3b
Michal Špaček <tupinek@gmail.com>
2015-03-01 15:09:34 +0100

    First stub.

---
+++ Image.pm
@@ -0,0 +1,56 @@
+package Lego::Part::Image;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+use Readonly;
+use Scalar::Util qw(blessed);
+
+# Constants.
+Readonly::Scalar our $EMPTY_STR => q{};
+
+# Version.
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;	
+
+	# Part object.
+	$self->{'part'} = undef;
+
+	# Process parameters.
+	set_params($self, @params);
+
+	# Check part object.
+	if (! defined $self->{'part'}) {
+		err "Parameter 'part' is required.";
+	}
+	if (! blessed($self->{'part'}) || ! $self->{'part'}->isa('Lego::Part')) {
+		err "Parameter 'part' must be Lego::Part object.";
+	}
+
+	# Object.
+	return $self;
+}
+
+# Get image.
+sub image {
+	return $EMPTY_STR;
+}
+
+# Get image URL.
+sub image_url {
+	return $EMPTY_STR;
+}
+
+1;
+
+__END__
