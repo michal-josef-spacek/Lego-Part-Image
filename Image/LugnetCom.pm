f67fc01261ec60f4c6ed86578524b4e53fe30ae8
Michal Špaček <tupinek@gmail.com>
2015-03-08 18:07:26 +0100

    Lego::Part::Image::LugnetCom: Add error check.

--- Image/LugnetCom.pm
+++ Image/LugnetCom.pm
@@ -5,12 +5,18 @@ use base qw(Lego::Part::Image);
 use strict;
 use warnings;
 
+# Modules.
+use Error::Pure qw(err);
+
 # Version.
 our $VERSION = 0.01;
 
 # Get image URL.
 sub image_url {
 	my $self = shift;
+	if (! defined $self->{'part'}->design_id) {
+		err "Design ID doesn't defined.";
+	}
 	my $url = sprintf 'http://img.lugnet.com/ld/%s.gif',
 		$self->{'part'}->design_id;
 	return $url;
