49e58ba07855ba7f84abe43b2bd87e30bfda43ee
Michal Špaček <tupinek@gmail.com>
2015-03-08 18:15:45 +0100

    Lego::Part::Image: Add doc.

--- Image.pm
+++ Image.pm
@@ -61,3 +61,108 @@ sub image_url {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Lego::Part::Image - Lego part image abstract class.
+
+=head1 SYNOPSIS
+
+ use Lego::Part::Image;
+ my $obj = Lego::Part::Image->new;
+ $obj->image;
+ $obj->image_url;
+
+=head1 METHODS
+
+=over 8
+
+=item * C<new()>
+
+ Constructor.
+ Returns object.
+
+=over 8
+
+=item * C<part>
+
+ Lego::Part object.
+ It is required.
+ Default value is undef.
+
+=back
+
+=item * C<image()>
+
+ Abstract method for getting image.
+
+=item * C<image_url()>
+
+ Abstract method for getting image url.
+
+=back
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Lego::Part;
+ use Lego::Part::Image;
+
+ # Error pure setting.
+ $ENV{'ERROR_PURE_TYPE'} = 'Print';
+
+ # Object.
+ my $obj = Lego::Part::Image->new(
+         'part' => Lego::Part->new(
+                'color' => 'red',
+                'design_id' => '3002',
+         ),
+ );
+
+ # Get image.
+ $obj->image;
+
+ # Output:
+ # Lego::Part::Image: This is abstract class. image() method not implemented.
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<Readonly>,
+L<Scalar::Util>.
+
+=head1 SEE ALSO
+
+L<Lego::Part::Image::PeeronCom>,
+L<Lego::Part::Image::LegoCom>,
+L<Lego::Part::Image::LugnetCom>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Lego-Part-Image>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
