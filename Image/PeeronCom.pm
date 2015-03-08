7a2c42fad72514b2aebb3f53f07396cc625d7f4c
Michal Špaček <tupinek@gmail.com>
2015-03-08 18:05:29 +0100

    Lego::Part::Imager::PeeronCom: Add error check.

--- Image/PeeronCom.pm
+++ Image/PeeronCom.pm
@@ -5,12 +5,21 @@ use base qw(Lego::Part::Image);
 use strict;
 use warnings;
 
+# Modules.
+use Error::Pure qw(err);
+
 # Version.
 our $VERSION = 0.01;
 
 # Get image URL.
 sub image_url {
 	my $self = shift;
+	if (! defined $self->{'part'}->color) {
+		err "Color doesn't defined.";
+	}
+	if (! defined $self->{'part'}->design_id) {
+		err "Design ID doesn't defined.";
+	}
 	my $url = sprintf 'http://media.peeron.com/ldraw/images/%d/100/%s.png',
 		$self->{'part'}->color, $self->{'part'}->design_id;
 	return $url;
